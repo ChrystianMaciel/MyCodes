75916b168f3e8804a584415966613faf222c7339
Chrystian Maciel (CmProphet) <75752709+ChrystianMaciel@users.noreply.github.com>
2021-01-21 17:29:06 -0300

    Update q25.c

--- q25.c
+++ q25.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int num1, num2;
-	num1 = 0;
-	num2 = 0;
-	scanf("%d %d", &num1, &num2);
-	
-	if(num1 % 3 == 0 && num1 % 5 == 0)
-	{
+int main(){
+
+    int n1 = 0, n2 = 0;
+
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+
+    if (n1 % 3 == 0 && n2 % 3 == 0)
+    {
         printf("sim\n");
     }
-	else if(num2 % 3 == 0 && num2 % 5 == 0)
-	{
-		printf("sim\n");
-	}	
-	else
+    else if (n1 % 5 == 0 && n2 % 5 == 0 )
     {
-        printf("nao\n");
+        printf ("sim\n");
+    }
+    else 
+    {
+        printf ("nao\n");
     }
 
-    return 0;
+    return (0);
 }
+
 
 
 /*#include <stdio.h>
